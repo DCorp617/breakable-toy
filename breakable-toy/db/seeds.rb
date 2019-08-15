218162ebefae41abd404de268f01d3d0dfa17100
DAVID CORPORAN <CORPORANDAVID@GMAIL.COM>
2019-08-15 16:48:26 -0400

    Heroku Debug Patch

--- breakable-toy/db/seeds.rb
+++
@@ -1,7 +0,0 @@
-Court.create([
-  {:name => 'Fallon Field', :street => '910 South St.', :city => 'Roslindale', :state => 'MA', :country => 'US', :latitude => '', :longitude => ''},
-  {:name => 'Truman Parkway', :street => '1101 Truman Pkwy', :city => 'Hyde Park', :state => 'MA', :country => 'US', :latitude => '', :longitude => ''},
-  {:name => 'Healy Field', :street => '4139 Washington St.', :city => 'Roslindale', :state => 'MA', :country => 'US', :latitude => '', :longitude => ''},
-  {:name => 'Boston College', :street => '100 St Thomas More Rd', :city => 'Boston', :state => 'MA', :country => 'US', :latitude => '', :longitude => ''},
-  {:name => 'Christopher Lee Playground', :street => '849 E 1st St', :city => 'Boston', :state => 'MA', :country => 'US', :latitude => '', :longitude => ''}
-])
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_131442) do
+ActiveRecord::Schema.define(version: 2019_08_05_175430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "courts", force: :cascade do |t|
     t.string "name", null: false
+    t.string "street", null: false
+    t.string "city", null: false
+    t.string "state", null: false
+    t.string "country", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "court_photo"
+    t.float "latitude"
+    t.float "longitude"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "court_id", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["court_id"], name: "index_reviews_on_court_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
